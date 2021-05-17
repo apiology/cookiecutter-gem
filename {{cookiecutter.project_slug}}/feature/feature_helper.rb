2558c5293f2d4de549bc2b2fc9ac19853546abfd
Vince Broz <vince@broz.cc>
2021-05-17 17:27:17 -0400

    Fix hard-coded project name

--- {{cookiecutter.project_slug}}/feature/feature_helper.rb
+++ {{cookiecutter.project_slug}}/feature/feature_helper.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 require 'open3'
-require 'bigfiles'
+require '{{cookiecutter.project_slug}}'
 
 # Add the bin directory, to allow testing of gem executables as if the gem is
 # already installed.
